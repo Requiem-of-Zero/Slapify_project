9c435cfafe1215b6b5b0ec32d70b613e16571047
Sam Wong <theesamwong@gmail.com>
2020-12-09 17:38:41 -0800

    added homepage sections

--- frontend/components/splash_page/logged_out_homepage.jsx
+++ frontend/components/splash_page/logged_out_homepage.jsx
@@ -7,8 +7,40 @@ class LoggedOutHomepage extends React.Component {
     return(
       <article>
       <Header />
-        <div className="row-items-1">
-          <div className="row-1">
+        <div className="row-items-1 row-items">
+          <div className="row-1 row">
+            <div className="row-content">
+              <h5>SLAPIFY PREMIUM</h5>
+              <h1>Get 1 month of Premium for free</h1>
+              <h3>Enjoy ad-free head-banging music, and much more. Cancel anytime.</h3>
+              <button className='apple-btn btn'>GET 1 MONTH FREE</button>
+              <p>Individual plan only. Only $5.00/month after.
+                Offer not available to users who already tried Premium.
+                <span><a href='#'>Terms and conditions apply.</a></span> Offer ends Dec 31, 2021.</p>
+            </div>
+            <div className="photo">
+              <img className="rave-photo" src={window.raveURL}/>
+            </div>
+          </div>
+        </div>
+        <div className="row-items-2 row-items">
+          <div className="row-2 row">
+            <div className="photo">
+              <img className="rave-photo" src={window.raveURL}/>
+            </div>
+            <div className="row-content">
+              <h5>SLAPIFY PREMIUM</h5>
+              <h1>Get 1 month of Premium for free</h1>
+              <h3>Enjoy ad-free head-banging music, and much more. Cancel anytime.</h3>
+              <button className='apple-btn btn'>GET 1 MONTH FREE</button>
+              <p>Individual plan only. Only $5.00/month after.
+                Offer not available to users who already tried Premium.
+                <span><a href='#'>Terms and conditions apply.</a></span> Offer ends Dec 31, 2021.</p>
+            </div>
+          </div>
+        </div>
+        <div className="row-items-3 row-items">
+          <div className="row-3 row">
             <div className="row-content">
               <h5>SLAPIFY PREMIUM</h5>
               <h1>Get 1 month of Premium for free</h1>
