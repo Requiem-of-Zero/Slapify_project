8cc02d1badef9f0c1891ee93d7ad531c3d081e96
Sam Wong <theesamwong@gmail.com>
2021-01-25 20:16:19 -0800

    gave musicc player redux state

--- frontend/reducers/music_player_reducer.js
+++ frontend/reducers/music_player_reducer.js
@@ -2,54 +2,133 @@ import
 { 
   RECEIVE_CURRENT_SONG,
   RECEIVE_QUEUE,
-  RECEIVE_PREVIOUS_SONG,
-  RECEIVE_NEXT_SONG,
   PAUSE_SONG,
   PLAY_SONG,
-  RECEIVE_SHUFFLE,
+  SHUFFLE_SONGS,
+  NEXT_SONG,
+  PREV_SONG,
+  LOOP_SONG,
+  pickSong
 } 
 from '../actions/music_player_actions';
 
-// const defaultState = {
-//   queue: [],
-//   previousTrack: [],
-//   shuffled:[],
-//   currentSongId: 17,
-//   playing: false
-// }
+const initialState = {
+  active: false,
+  queue: [],
+  startIdx: 0,
+  played: [],
+  currentSongId: null,
+  playing: false,
+  loop: false,
+  shuffle: false,
+};
 
-const MusicPlayerReducer = (oldState={}, action) => {
+
+
+const MusicPlayerReducer = (oldState = initialState, action) => {
   Object.freeze(oldState);
-  let newState = Object.assign({}, oldState)
+  let newState = { ...oldState }
 
   switch (action.type) {
-    case RECEIVE_CURRENT_SONG:
-      // newState.currentSongId = action.songId;
-      return action.currentSong
     case RECEIVE_QUEUE:
-      newState.queue = Object.keys(action.queue)
-      return newState
-    case RECEIVE_PREVIOUS_SONG:
-      if(!newState.previousTrack.includes(action.songId)) newState.previousTrack.push(action.songId)
-    case RECEIVE_NEXT_SONG:
-      newState.queue.unshift(action.songId);
-      return newState
+      newState.active = true;
+      newState.queue = action.queue;
+      newState.startIdx = 0;
+      newState.currentSong = newState.queue[newState.startIdx];
+      newState.playing = true;
+      return newState;
+
+    case PREV_SONG:
+      if (oldState.active) {
+        newState.playing = true;
+        if(state.played.length > 0) {
+          let newPlayed = oldState.played
+          let lastIdx = newPlayed.pop();
+          newState.played = newPlayed;
+
+          newState.startIdx = lastIdx;
+
+          newState.currentSongId = oldState.queue[newState.startIdx];
+          return newState;
+        } else {
+          newState.active = false;
+          newState.currentTrack = null;
+          newState.playing = false;
+          return newState;
+        }
+      } else {
+        return newSTate
+      }
+
+    case NEXT_SONG:
+      if(oldState.active) {
+        newState.playing = true;
+        if(oldState.played.length < oldState.queue.length - 1) {
+          let newPlayed = oldState.played;
+          newPlayed.push(oldState.startIdx);
+          newState.played = newPlayed;
+
+          if(oldState.shuffle) {
+            newState.startIdx = pickSong(
+              oldState.queue.length,
+              newPlayed
+            )
+          } else {
+            newState.startIdx += 1;
+          }
+
+          newState.currentSong = oldState.queue[newState.startIdx];
+          return newState;
+        } else {
+          newState.startIdx = 0;
+          newState.played = [];
+
+          if(oldState.loop) {
+            newState.currentSongId = state.queue[newState.startIdx];
+            return newState
+          } else {
+            newState.active = false;
+            newState.currentSongId = null;
+            newState.playing = false;
+            return newState;
+          }
+        }
+      } else {
+        return newState;
+      }
+
     case PAUSE_SONG:
       newState.playing = false;
       return nextState
-    case PLAY_SONG:
-      newState.playing = true;
-      return newState
-    case RECEIVE_SHUFFLE:
-      const songs = Object.values(action.songs);
 
-      for(let i=0; i < songs.length - 1; i++) {
-        let randomNum = Math.floor(Math.random() * (i + 1));
-        [songs[i], songs[randomNum]] = [songs[randomNum], songs[i]];
+    case PLAY_SONG:
+      if(state.queue.length) {
+        newState.active = true;
+        newState.currentSongId = newState.queue[newState.startIdx];
+        newState.playing = true;
+        return newState;
+      } else {
+        return newState
       }
 
-      songs.forEach( song => newState.shuffled.push(song.id));
+    case LOOP_SONG:
+      newState.loop = !oldState.loop;
+      return newState;
+
+    case SHUFFLE_SONGS:
+      newState.shuffle = !oldState.shuffle;
       return newState
+
+    // case RECEIVE_SHUFFLE:
+    //   const songs = Object.values(action.songs);
+
+    //   for(let i=0; i < songs.length - 1; i++) {
+    //     let randomNum = Math.floor(Math.random() * (i + 1));
+    //     [songs[i], songs[randomNum]] = [songs[randomNum], songs[i]];
+    //   }
+
+    //   songs.forEach( song => newState.shuffled.push(song.id));
+    //   return newState
     default:
       return oldState
   }
