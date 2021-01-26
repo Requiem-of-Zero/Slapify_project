5ac24ff3c0b4bbb6d31f88cb1d8ff12244230d32
Sam Wong <theesamwong@gmail.com>
2021-01-25 20:17:05 -0800

    added line for songs payload

--- frontend/reducers/albums_reducer.js
+++ frontend/reducers/albums_reducer.js
@@ -2,13 +2,14 @@ import { RECEIVE_ALL_ALBUMS, RECEIVE_ALBUM } from '../actions/album_actions';
 
 const AlbumsReducer = (oldState={}, action) => {
   Object.freeze(oldState);
-  let newState = Object.assign({}, oldState)
+  let newState = { ...oldState }
 
   switch(action.type) {
     case RECEIVE_ALL_ALBUMS:
       return action.albums
     case RECEIVE_ALBUM:
       newState[action.album.id] = action.album
+      newState[action.album.songs] = action.album.songs
       return newState
     default:
       return oldState
