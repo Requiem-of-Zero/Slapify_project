667aada85b0109935a7afab84bb0273d0be9307c
Sam Wong <theesamwong@gmail.com>
2021-01-29 19:07:22 -0800

    new props to use queue

--- frontend/components/albums/album_show_container.js
+++ frontend/components/albums/album_show_container.js
@@ -2,20 +2,21 @@ import { getAlbum } from '../../actions/album_actions';
 import { connect } from 'react-redux';
 import AlbumShow from './album_show';
 import { getSongsByAlbumId } from '../../reducers/selectors';
-import { updateCurrentSong } from '../../actions/music_player_actions';
+import { receiveQueue, playSong, pauseSong } from '../../actions/music_player_actions';
 import { getAllArtists } from '../../actions/artist_actions';
 
 const mstp = (state, ownProps) => ({
   album: state.entities.albums[ownProps.match.params.albumId],
   songs: getSongsByAlbumId(state, +ownProps.match.params.albumId),
   currentUser: state.entities.users[state.session.id],
-  artists: state.entities.artists
+  artists: state.entities.artists,
+  playing: state.music.playing
 });
 
 const mdtp = dispatch => ({
   getAlbum: albumId => dispatch(getAlbum(albumId)),
-  updateCurrentSong: song => dispatch(updateCurrentSong(song)),
-  getAllArtists: () => dispatch(getAllArtists())
+  getAllArtists: () => dispatch(getAllArtists()),
+  receiveQueue: queue => dispatch(receiveQueue(queue)),
 });
 
 export default connect(mstp, mdtp)(AlbumShow);
