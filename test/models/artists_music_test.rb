78551ef94792497e26b04aab5a65a5e3631f7380
Sam Wong <theesamwong@gmail.com>
2021-02-02 00:50:14 -0800

    rails files auto generated

--- test/models/artists_music_test.rb
+++ test/models/artists_music_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: artists_musics
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  album_id   :integer          not null
+#  artist_id  :integer          not null
+#
+# Indexes
+#
+#  index_artists_musics_on_album_id   (album_id)
+#  index_artists_musics_on_artist_id  (artist_id)
+#
 require 'test_helper'
 
 class ArtistsMusicTest < ActiveSupport::TestCase
