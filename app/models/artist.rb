e6cd4f737bbac8b6773a6f04c26d138d68fa6f0a
Sam Wong <theesamwong@gmail.com>
2021-02-02 00:49:02 -0800

    annotated models

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: artists
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_artists_on_name  (name) UNIQUE
+#
 class Artist < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
