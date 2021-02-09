32b352bf1276a1b5dd96eb3172ba28dfc4c07734
Sam Wong <theesamwong@gmail.com>
2021-02-08 21:13:21 -0800

    created API util for playlists

---
+++ frontend/util/playlist_api_util.js
@@ -0,0 +1,48 @@
+export const createPlaylist = playlist => (
+  $.ajax({
+    url: '/api/playlists',
+    method: "POST",
+    data: { playlist }
+  })
+);
+
+export const updatePlaylist = playlist => (
+  $.ajax({
+    url: `/api/playlists/${playlist.id}`,
+    method: "PATCH",
+    data: { playlistName: playlist.playlist_name }
+  })
+);
+
+export const removePlaylist = playlistId => (
+  $.ajax({
+    url: `/api/playlists/${playlistId}`,
+    method: "DELETE"
+  })
+);
+
+export const receiveUserPlaylists = () => (
+  $.ajax({
+    url: `/api/playlists`,
+  })
+);
+
+export const receivePlaylist = playlistId => (
+  $.ajax({
+    url: `/api/playlists/${playlistId}`
+  })
+);
+
+export const addSongToPlaylist = (songId, playlistId) => (
+  $.ajax({
+    url: `/api/playlists/${playlistId}/songs/${songId}`,
+    method: "POST"
+  })
+);
+
+export const removeSongFromPlaylist = (songId, playlistId) => (
+  $.ajax({
+    url: `/api/playlists/${playlistId}/songs/${songId}`,
+    method: "DELETE"
+  })
+);
