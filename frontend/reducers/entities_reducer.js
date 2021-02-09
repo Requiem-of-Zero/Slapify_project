9568ec1ac2d489ddccd6b1a8489630b48dd5005e
Sam Wong <theesamwong@gmail.com>
2021-02-08 21:13:49 -0800

    added playlists to entities slice of state

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -3,12 +3,14 @@ import UsersReducer from "./users_reducer";
 import SongsReducer from "./songs_reducer";
 import AlbumsReducer from "./albums_reducer";
 import ArtistsReducer from './artists_reducer';
+import PlaylistsReducer from './playlists_reducer';
 
 const EntitiesReducer = combineReducers({
   users: UsersReducer,
   songs: SongsReducer,
   albums: AlbumsReducer,
   artists: ArtistsReducer,
+  playlists: PlaylistsReducer
 });
 
 export default EntitiesReducer;
